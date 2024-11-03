3ac6e358b11535a1d26496dc5a37eaf8cdfa9cf1
Aielina <aielina.madambekova@gmail.com>
2024-11-03 22:07:49 +0600

    Added MealsContainer and display basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import MealsContainer from './containers/MealsContainer/MealsContainer.tsx';
+
 const App = () => {
   return (
     <div>
-
+      <MealsContainer />
     </div>
   );
 };
---
+++ src/containers/MealsContainer/MealsContainer.tsx
@@ -0,0 +1,12 @@
+
+
+const MealsContainer = () => {
+  return (
+    <div className='container mt-5'>
+      <h1>Calorie Tracker</h1>
+      <button>Add new meal</button>
+    </div>
+  );
+};
+
+export default MealsContainer;
